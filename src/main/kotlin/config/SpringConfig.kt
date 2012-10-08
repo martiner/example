1911857df0a58f07a67582e1ad511a2279ffff3a
Martin Caslavsky <mcaslavsky@gmail.com>
2012-10-08 18:42:42 +0200

    add: skeleton application

---
+++ src/main/kotlin/config/SpringConfig.kt
@@ -0,0 +1,9 @@
+package config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Bean
+
+[Configuration]
+public open class SpringConfig {
+
+}
---
+++ src/main/kotlin/config/WebInitializer.kt
@@ -0,0 +1,25 @@
+package config
+
+import javax.servlet.ServletContext
+import org.springframework.web.WebApplicationInitializer
+import org.springframework.web.context.ContextLoaderListener
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
+import org.springframework.web.servlet.DispatcherServlet
+
+public class WebInitializer: WebApplicationInitializer {
+
+    public override fun onStartup(servletContext: ServletContext?) {
+        val sc = servletContext!!
+
+        val root = AnnotationConfigWebApplicationContext()
+        root.register(javaClass<SpringConfig>())
+        sc.addListener(ContextLoaderListener(root));
+
+        val ctx = AnnotationConfigWebApplicationContext()
+        ctx.register(javaClass<SpringWebConfig>())
+        val servlet = sc.addServlet("appServlet", DispatcherServlet(ctx))!!
+        servlet.setLoadOnStartup(1)
+        servlet.addMapping("*.go")
+
+    }
+}
